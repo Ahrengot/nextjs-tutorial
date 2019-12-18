0b301a8f0c3c2e5b25a38f3ab562132d66198034
Jens Ahrengot Boddum <ahrengot@gmail.com>
2019-12-18 22:51:42 +0100

    Remove .next folder from version control

--- components/GlobalHead.tsx
+++ components/GlobalHead.tsx
@@ -6,7 +6,6 @@ const linkStyle = {
 
 const GlobalHead = () => (
   <head>
-    <title>Next.js tutorial</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
   </head>

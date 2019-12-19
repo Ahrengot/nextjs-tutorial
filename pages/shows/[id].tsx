8b670ce8cff4607a2515fb3ab7ea96c742e23621
Jens Ahrengot Boddum <ahrengot@gmail.com>
2019-12-19 02:21:04 +0100

    Add cast details to shows

--- pages/shows/[id].tsx
+++ pages/shows/[id].tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import PageLayout from "../../components/PageLayout";
 import fetch from "isomorphic-unfetch";
 
-import { ShowData } from "../../types/show";
+import { ShowData, CastMember } from "../../types/show";
 import { NextSeo } from "next-seo";
+import { useState, useEffect } from "react";
 
 type Props = {
   show? : ShowData;
@@ -11,6 +12,18 @@ type Props = {
 }
 
 const SingleShowPage = ({show, showId}:Props) => {
+  const [cast, setCast] = useState<CastMember[]>([]);
+
+  useEffect(() => {
+    if (show) {
+      setCast([]);
+
+      fetch(`http://api.tvmaze.com/shows/${show.id}/cast`)
+        .then(res => res.json())
+        .then(setCast);
+    }
+  }, [show])
+
   if (!show) {
     return (
       <PageLayout>
@@ -24,15 +37,11 @@ const SingleShowPage = ({show, showId}:Props) => {
     <PageLayout>
       <NextSeo title={`${show.name} | TV Shows`} />
       <h1 className="display-4 mb-4">{show.name}</h1>
-      <div className="row">
+      <section className="row">
         <div className="col-6 col-lg-4">
-          {show.image && show.image.original && <img src={show.image.original} alt={show.name} />}
-          <style jsx>{`
-            img {
-              max-width: 100%;
-              height: auto;
-            }
-          `}</style>
+          {show.image && show.image.original && (
+            <img src={show.image.original} alt={show.name} className="img-fluid shadow" />
+          )}
         </div>
         <div className="col-6 col-lg-8">
           <section className="mb-3 bg-light rounded py-3 px-4">
@@ -55,7 +64,25 @@ const SingleShowPage = ({show, showId}:Props) => {
             `}</style>
           </section>
         </div>
-      </div>
+      </section>
+      {cast.length > 0 && (
+        <section className="mt-5">
+          <h2 className="display-6 mb-3">Cast members</h2>
+          <div className="person-grid">
+            {cast.map(({ person, character }) => (
+              <a href={person.url} className="person-grid-item" key={person.id} target="_blank">
+                {person.image.medium && (
+                  <figure>
+                    <img src={person.image.medium} alt={person.name} className="img-responsive rounded-circle" />
+                  </figure>
+                )}
+                <h4>{person.name}</h4>
+                <h6>{character.name}</h6>
+              </a>
+            ))}
+          </div>
+        </section>
+      )}
     </PageLayout>
   );
 };
--- types/show.ts
+++ types/show.ts
@@ -1,24 +1,40 @@
 export type ShowData = {
-    id: number,
-    url: string,
-    name: string,
-    genres: string[],
-    premiered: string,
-    rating: {
-      average: number,
-    },
-    image?: {
-      medium?: string,
-      original?: string,
-    },
-    summary: string,
-    network?: {
-      country: {
-        name: string,
-        code: string,
-      }
+  id: number,
+  url: string,
+  name: string,
+  genres: string[],
+  premiered: string,
+  rating: {
+    average: number,
+  },
+  image?: {
+    medium?: string,
+    original?: string,
+  },
+  summary: string,
+  network?: {
+    country: {
+      name: string,
+      code: string,
     }
   }
+}
+
+export type Persona = {
+  id: number,
+  url: string,
+  name: string,
+  image: {
+    medium?: string,
+  }
+}
+
+export type CastMember = {
+  person: Persona,
+  character: Persona,
+  self: boolean,
+  voice: boolean,
+}
 
 export type Show = {
   score: number,
